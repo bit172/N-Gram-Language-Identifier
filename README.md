eba1775d847f907e77242d34e3b9f00f2c5cc12c
Jonathan Hsu <bit172@gmail.com>
2020-04-04 17:33:30 -0400

    Add trace files, Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # COMP472-Project2
+https://github.com/bit172/COMP472-Project2
+
+# Instructions
+1. Log in to the linux environment
+2. Open the terminal
+3. Run `git clone https://github.com/bit172/COMP472-Project2`
+4. `cd` into the project folder
+5. Edit the `input.txt` file and add hyper-parameters, training and test files
+6. Run `module load anaconda3`
+7. Run `python3 main.py`
+8. See output files in `results/`
--- main.py
+++ main.py
@@ -5,21 +5,25 @@ from unigram import Unigram
 from bigram import Bigram
 from trigram import Trigram
 
-inputs = read('input.txt')[0].split(" ")
+inputs = read('input.txt')[0].strip().split(" ")
 V, N, S_FACTOR, TRAINING_FILE, TEST_FILE = (int(inputs[0]), int(inputs[1]), float(inputs[2]), inputs[3], inputs[4])
 OUTPUT_FILE_NAME = f"./results/trace_{V}_{N}_{S_FACTOR}.txt"
 
 t1 = time()
 if V == 3:
+    print(f"BYOM: V = {V} n = 3 d = {S_FACTOR}")
     BYOM = BYOM(V, S_FACTOR, TRAINING_FILE, TEST_FILE, OUTPUT_FILE_NAME)
     BYOM.execute()
 elif N == 1:
+    print(f"unigram: V = {V} d = {S_FACTOR}")
     UNIGRAM = Unigram(V, S_FACTOR, TRAINING_FILE, TEST_FILE, OUTPUT_FILE_NAME)
     UNIGRAM.execute()
 elif N == 2:
+    print(f"bigram: V = {V} d = {S_FACTOR}")
     BIGRAM = Bigram(V, S_FACTOR, TRAINING_FILE, TEST_FILE, OUTPUT_FILE_NAME)
     BIGRAM.execute()
 elif N == 3:
+    print(f"trigram: V = {V} d = {S_FACTOR}")
     TRIGRAM = Trigram(V, S_FACTOR, TRAINING_FILE, TEST_FILE, OUTPUT_FILE_NAME)
     TRIGRAM.execute()
 t2 = time()
